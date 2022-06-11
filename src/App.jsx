4d2b01b31f6114ffd14dc886f47bdf2f6b750a18
Rahul Raut <rkr2137@gmail.com>
2022-06-11 21:07:26 +0530

    updated recent events

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ const App = () => {
     <div>
       <Navigation />
       <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
+      <Features data={landingPageData.Features} />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery}/>
       {/* <Testimonials data={landingPageData.Testimonials} /> */}
