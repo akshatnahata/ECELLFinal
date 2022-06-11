0e9cd3ff6cb6f615ac453407ff48caf453bdcc38
Rahul Raut <rkr2137@gmail.com>
2022-06-11 23:34:55 +0530

    added the card for teams

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,15 +4,15 @@ export const About = (props) => {
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/ENGEN-01.jpg" className="img-responsive" alt="" />
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
               <h3>Vision</h3>
-              <div className="list-style">
+              <p style={{fontStyle : "italic" }}>{props.data ? props.data.vision : "loading..."}</p>
+              {/* <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
@@ -31,7 +31,7 @@ export const About = (props) => {
                       : "loading"}
                   </ul>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
--- src/components/ourteam.jsx
+++ src/components/ourteam.jsx
@@ -2,8 +2,8 @@ export const Ourteam = (props) => {
     return (
       <div id='ourteam' className='text-center'>
         <div className='container'>
-          <div className='col-md-8 col-md-offset-2 section-title'>
-            <h2>Our Investors and Mentors</h2>
+          <div className='col-md-8 col-md-offset-2 section-title' style={{marginBottom: "20px"}}>
+            <h2>Our Team</h2>
             {/* <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
               dapibus leonec.
@@ -12,16 +12,35 @@ export const Ourteam = (props) => {
           <div id='row'>
             {props.data
               ? props.data.map((d, i) => (
-                  <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 mentor'>
-                    <div className='thumbnail'>
-                      {' '}
-                      <img src={d.img} alt='...' className='team-img' />
-                      <div className='caption'>
-                        <h4>{d.name}</h4>
-                        <p>{d.job}</p>
-                      </div>
-                    </div>
+                <div class="card-container">
+                  {/* <span class="pro">PRO</span> */}
+                  <img
+                    class="round"
+                    src={d.img}
+                    alt="user"
+                  />
+                  <h3>{d.name}</h3>
+                  <h6>{d.res}</h6>
+                  <div class="skills">
+                  <ul>
+                  <li>
+                    <a href={d.facebook ? d.facebook : '/'}>
+                      <i className='fa fa-facebook'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={d.twitter ? d.twitter : '/'}>
+                      <i className='fa fa-twitter'></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={d.youtube ? d.youtube : '/'}>
+                      <i className='fa fa-youtube'></i>
+                    </a>
+                  </li>
+                </ul>
                   </div>
+                </div>
                 ))
               : 'loading'}
           </div>
