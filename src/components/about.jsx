1019a0b1f43a42dc132da7b58e29cc68cea64979
Abhinav Yadav <actcrick02@gmail.com>
2022-06-12 22:35:31 +0530

    header and about us animated

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,17 +1,31 @@
+import Fade from 'react-reveal/Fade';
+
 export const About = (props) => {
   return (
     <div id="about">
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-6">
-            <img src="img/ENGEN-01.jpg" className="img-responsive" alt="" />
+            <Fade bottom>
+              <img src="img/ENGEN-01.jpg" className="img-responsive" alt="" />
+            </Fade>
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Vision</h3>
-              <p style={{fontStyle : "italic" }}>{props.data ? props.data.vision : "loading..."}</p>
+              <h2><Fade left cascade>
+                About Us</Fade></h2>
+              <p><Fade right>{props.data ? props.data.paragraph : "loading..."}
+              </Fade></p>
+              <h3>
+                <Fade left cascade>
+                  Vision
+                </Fade>
+              </h3>
+              <p style={{ fontStyle: "italic" }}>
+                <Fade right>
+                  {props.data ? props.data.vision : "loading..."}
+                </Fade>
+              </p>
               {/* <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,4 @@
+import { Zoom, Fade } from 'react-reveal';
 export const Header = (props) => {
   return (
     <header id='header'>
@@ -6,20 +7,22 @@ export const Header = (props) => {
           <div className='container'>
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
-                <h1>
-                  {props.data ? props.data.title : 'Loading'}
-                  <br/>
-                  {/* {props.data ? props.data.subtitle : 'Loading' } */}
-                  <span></span>
-                </h1>
-                <p id="IITI">{props.data ? props.data.paragraph : 'Loading'}</p>
-                <p id="quote">"{props.data ? props.data.paragraph2 : 'Loading'}"</p>
-                <a
-                  href='#about'
-                  className='btn btn-custom btn-lg page-scroll'
-                >
-                  Learn More
-                </a>{' '}
+                <Fade bottom>
+                  <h1>
+                    {props.data ? props.data.title : 'Loading'}
+                    <br />
+                    {/* {props.data ? props.data.subtitle : 'Loading' } */}
+                    <span></span>
+                  </h1>
+                  <p id="IITI">{props.data ? props.data.paragraph : 'Loading'}</p>
+                  <p id="quote">"{props.data ? props.data.paragraph2 : 'Loading'}"</p>
+                  <a
+                    href='#about'
+                    className='btn btn-custom btn-lg page-scroll'
+                  >
+                    Learn More
+                  </a>{' '}
+                </Fade>
               </div>
             </div>
           </div>
