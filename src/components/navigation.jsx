84e3822fa09a2cd9d73e72123f83f88843f2b856
akshatnahata <akshatnahata05@gmail.com>
2022-06-16 21:30:25 +0530

    logo at left

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,6 @@
 export const Navigation = (props) => {
   return (
-    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top '>
       <div className='container-nav'>
         <div className='navbar-header'>
           <button
