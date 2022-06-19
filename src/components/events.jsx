2384c413c162c0569fa422750490a927885f6feb
Krish Agrawal <diwankrish.17@gmail.com>
2022-06-19 12:15:18 +0530

    Reponsive Navbar, Header, About, Events, WhatWeDo

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -8,7 +8,7 @@ export const Events = (props) => {
         <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+                <div key={`${d.title}-${i}`} className='col-xs-12 col-sm-6 col-md-3'>
                   {' '}
                   <i className={d.icon}></i>
                   <h3>{d.title}</h3>
